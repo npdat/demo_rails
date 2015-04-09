fa21a065f2983c1831141654e40cc8acace25008
npdat <npdat@tma.com.vn>
2015-04-09 16:44:51 +0700

    Update user_spec.rb
    update datslife 111

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
   context "with 2 or more Users" do
     it "orders them in reverse" do
-      user = User.create(username: "datslife", email: "npdat@tma.com.vn")
+      user = User.create(username: "datslife111", email: "npdat@tma.com.vn")
       #user1 = user.username("user1")
       #user2 = user.username("user2")
       #user.reload.username.should eq("datslife")
